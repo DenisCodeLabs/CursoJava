3c465898f405b3b5af76f8535ede020265063332
AlexGuido <alexanderguido@outlook.com>
2024-10-10 23:31:46 -0600

    Operadores Logicos con array

--- variables/src/OperadoresLogicosLogin.java
+++ variables/src/OperadoresLogicosLogin.java
@@ -3,27 +3,37 @@ import java.util.Scanner;
 public class OperadoresLogicosLogin {
     public static void main(String[] args) {
 
-        String username = "denis";
-        String password = "12345";
+        /*String []usernames = new String[3];
+        String [] passwords = new String[3];
 
-        String username2 = "admin";
-        String password2 = "123456";
+        usernames[0] = "denis";
+        usernames[1] = "admin";
+        usernames[2] = "root";
 
+        passwords[0] = "12345";
+        passwords[1] = "123456";
+        passwords[2] = "root";*/
+
+        String []usernames = {"denis", "admin", "root"};
+        String []passwords = {"12345", "123456", "root"};
 
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Ingrese el username");
         String usuario = scanner.next();
-
         System.out.println("Ingrese el password");
         String pass = scanner.next();
 
         boolean isAuthentic = false;
-        if( username.equals(usuario) && password.equals(pass) || username2.equals(usuario) && password2.equals(pass)){
-            System.out.println("Bienvenido ".concat(usuario));
-            isAuthentic = true;
-        } else{
-            System.out.println("Datos incorrectos");
+        for(int i = 0; i < usernames.length; i++){
+
+            if(usernames[i].equals(usuario) && passwords[i].equals(pass)){
+                System.out.println("Bienvenido ".concat(usuario));
+                isAuthentic = true;
+                break;
+            }
         }
+        if(isAuthentic == false)
+            System.out.println("Datos incorrectos");
     }
 }
